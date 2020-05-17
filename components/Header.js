54f1597cfe39e2e2e02641dcf11dc8d2fc5ea69b
Vitaly Liber <zenamax@gmail.com>
2020-05-18 00:31:12 +0300

    icons was replaced

--- components/Header.js
+++ components/Header.js
@@ -2,9 +2,9 @@ import Head from "next/head";
 import Link from "next/link";
 import colors from "../utils/colors";
 
-const title = "Otaku Shop";
+const title = "Magatama";
 const desc =
-  "Otaku Shop is a great place to get your anime merchandise, cosplay costumes, props and accessories.";
+  "Great place to get your anime merchandise, props and accessories.";
 const Header = () => {
   return (
     <>
@@ -14,19 +14,19 @@ const Header = () => {
         <link
           rel="shortcut icon"
           type="image/x-icon"
-          href="https://juniors.casply.com/favicon.ico"
+          href="https://magatama.casply.com/favicon.ico"
         />
         <link
           rel="apple-touch-icon"
           type="image/png"
-          href="https://juniors.casply.com/favicon.png"
+          href="https://magatama.casply.com/favicon.png"
         />
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={desc} />
         <meta name="twitter:card" content="summary_large_image" />
         <meta
           name="twitter:image"
-          content="https://juniors.casply.com/twitter-card.jpg"
+          content="https://magatama.casply.com/twitter-card.jpg"
         />
       </Head>
 
@@ -34,7 +34,7 @@ const Header = () => {
         <div className="container justify-content-between">
           <Link href="/" as="/">
             <a className="navbar-brand text-white">
-              <span>Otaku</span> Shop
+              <span>Magatama</span> Shop
             </a>
           </Link>
         </div>
